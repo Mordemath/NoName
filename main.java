374068f9c8ec039f2b82b01a8854fe64206b2a48
LucianoSigot <lucianosigot@gmail.com>
2025-04-17 16:07:13 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,29 @@
-public class main{
- public static void main(String[] args) {
-    
- }
+import java.time.LocalDateTime;
+public class main {
+    public static void main(String[] args) {
+        GestionStock stock = new GestionStock(10, 123);
+        stock.GenerarInforme();
+        stock.Notificaciónes();
+
+        Libro libro1 = new Libro("Java Básico", 111222, "Autor X", 300, Libro.DISPONIBLE, 1, "Editorial Y");
+        System.out.println("Libro creado: " + libro1.getTitulo());
+
+        Bibliotecario bib = new Bibliotecario("Ana", "pass", LocalDateTime.now(), "ana@mail.com", 1);
+        bib.pedirReserva();
+
+        Cliente cli = new Cliente("Luis", "pwd", LocalDateTime.now(), "luis@mail.com", 2);
+        cli.buscarLibro();
+
+        Reserva res = new Reserva(1, 0.0, LocalDateTime.now(), LocalDateTime.now().plusDays(7));
+        res.ticketReserva();
+
+        Transacciones tx = new Transacciones(150.0, LocalDateTime.now(), 1);
+        tx.imprimirFactura();
+        tx.descargarPago();
+
+        Proveedores prov = new Proveedores("http://proveedor.com", "555-1234", "Prov1", "prov1@mail.com", 10);
+        prov.mostrarCatalago();
+        prov.venderLibro();
+        prov.generarReciboDelPedido();
+    }
 }
