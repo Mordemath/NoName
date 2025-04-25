150cfb5e83fa7c6c458424db7d1a7e50b3093229
Mordemath <165727141+Mordemath@users.noreply.github.com>
2025-04-25 13:26:06 -0300

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -5,10 +5,7 @@ public class Reserva {
     private double monto;             // R-Monto
     private LocalDateTime fechaReserva;             // R-Fecha
     private LocalDateTime fechaDevolucion;   // R-Fecha Devolución
-    private int idCliente;
-    private int idTransaccion;
-    private int idBibliotecario;
-    private int isbnLibro;
+
     // Constructor
     public Reserva(int id, double monto, LocalDateTime fecha, LocalDateTime fechaDevolucion, int idCliente, int idBibliotecario, int idTransaccion, int isbnLibro) {
         this.id = id;
